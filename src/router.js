b463728b1d36cfbe96d6e78592125b8b14b71f53
mvppcf <adokenny@qq.com>
2019-02-21 10:14:38 +0800

    'vueproject'

---
+++ src/router.js
@@ -0,0 +1,11 @@
+
+import VueRouter from 'vue-router';
+
+// 3. 创建路由对象
+let router = new VueRouter({
+    routes : [
+        
+    ]
+});
+
+export default router;
---
+++ src/test.js
@@ -0,0 +1,10 @@
+let info = {
+    name: 'ling',
+    age: 19
+}
+
+export default info;
+
+
+export var title = '小星星'
+export var content = '哈哈哈'
